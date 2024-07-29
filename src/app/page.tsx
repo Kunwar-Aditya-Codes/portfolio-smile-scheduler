4a212ee1eab218c3392e874b97eb8d35dacc4cb3
Kunwar Aditya <adityakunwar0147@gmail.com>
2024-07-29 10:54:15 +0530

    ui: hero section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,47 +12,64 @@ const dancing = Dancing_Script({
 
 const Home = () => {
   return (
-    <div className='min-h-screen  bg-teal-800'>
-      <div className='h-screen  flex items-start justify-between'>
-        <div className='h-full flex-[0.5]  p-16 pl-24'>
+    <div className='min-h-screen  bg-cyan-500/30'>
+      <div className='h-screen max-h-screen overflow-y-hidden flex items-start justify-between'>
+        <div className='h-full flex-[0.5]  ml-4 p-16 pl-24'>
           <h1
             className={cn(
-              ' text-teal-300 text-5xl font-black rounded-xl',
+              ' text-cyan-800 text-5xl font-black rounded-xl',
               dancing.className
             )}
           >
             Smile Scheduler
           </h1>
-          <div className='mt-8 max-w-[40rem]'>
-            <h2 className='text-[7rem] mt-4 leading-[7.5rem]  font-medium tracking-tighter text-white '>
+          <div className='mt-8 max-w-[40rem] h-full'>
+            <h2 className='text-[7rem] mt-4 leading-[7.5rem]  font-medium tracking-tighter text-black '>
               Your smile comes first
             </h2>
 
-            <p className='mt-4 p-2 tracking-tight leading-7 text-zinc-200 text-lg pr-4 text-justify '>
-              Appointment booking to your nearest dental clinic made easy and
-              hassle free. So no waiting in those long queues. Just book and
-              arrive on time.
-            </p>
+            <div className=''>
+              <p className='mt-4 p-2 tracking-tight leading-7 text-zinc-700 font-medium text-lg pr-4 text-justify '>
+                Appointment booking to your nearest dental clinic made easy and
+                hassle free. So no waiting in those long queues. Just book and
+                arrive on time.
+              </p>
 
-            <Button
-              className='mt-12 rounded-full'
-              size={'lg'}
-              variant={'secondary'}
-            >
-              Book Appointment <ArrowRight className='size-4 ml-1' />
-            </Button>
+              <Button
+                variant={'default'}
+                className='mt-12 rounded-full'
+                size={'lg'}
+              >
+                Book Appointment <ArrowRight className='size-4 ml-1' />
+              </Button>
+            </div>
           </div>
         </div>
-        <div className='flex-[0.5]   h-full flex justify-end   '>
+        <div className='flex-[0.5]  h-full flex justify-end scale-95 '>
           <Image
             src={'/2.png'}
             width={500}
             height={500}
             alt='hero-image'
-            className='h-full object-cover w-[35rem] pt-12'
+            className='h-full object-cover w-[35rem] pt-12 mr-24 bg-white/60 rounded-b-full rounded-t-full mt-0.5  '
           />
         </div>
       </div>
+
+      <MaxWidthWrapper className='pb-12 mt-8'>
+        <div className='bg-white/70 shadow-md  rounded-[3rem] p-4'>
+          <div className='p-6 grid gap-y-8'>
+            <div className='bg-cyan-500 text-white rounded-[2.5rem] flex items-center justify-center text-center px-4 text-3xl  w-[15rem] h-[15rem]'>
+              25+ years of experienced doctors
+            </div>
+            <div className='bg-cyan-500 text-white rounded-[2.5rem] flex items-center justify-center text-center px-4 text-3xl  w-[15rem] h-[15rem]'>
+              25+ years of experienced doctors
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </MaxWidthWrapper>
     </div>
   );
 };
